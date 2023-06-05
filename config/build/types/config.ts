37800f731a11c1ce7dcb5904e70a60827fb652cb
kirkol <kirkol94@ya.ru>
2023-06-05 12:56:29 +0500

    helper собственный classNames

---
+++ config/build/types/config.ts
@@ -0,0 +1,19 @@
+export type BuilMode = "production" | "development";
+
+export interface BuildPaths {
+  entry: string;
+  build: string;
+  html: string;
+}
+
+export interface BuildEnv {
+  mode: BuilMode;
+  port: number;
+}
+
+export interface BuildOptions {
+  mode: BuilMode;
+  paths: BuildPaths;
+  isDev: boolean;
+  port: number;
+}
--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,13 @@ import './styles/index.scss'
 import { AboutPageAsync } from './pages/AboutPage/AboutPageAsync'
 import { MainPageAsync } from './pages/MainPage/MainPageAsync'
 import { useTheme } from './theme/useTheme'
+import { classNames } from './helpers/classNames/classNames'
 
 export const App = () => {
     const { theme, toggleTheme } = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <ul className='nav'>
                 <li>
                     <Link to=''>main</Link>
