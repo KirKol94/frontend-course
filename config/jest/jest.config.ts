2bf5d91f93c9cac703c4f45fb492734b5c4c251a
kirkol <kirkol94@ya.ru>
2023-06-09 15:09:26 +0500

    Настроил абсолютные импорты для jest

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -12,6 +12,9 @@ export default {
     moduleDirectories: [
         'node_modules',
     ],
+    modulePaths: [
+        '<rootDir>src',
+    ],
     moduleFileExtensions: [
         'js',
         'mjs',
